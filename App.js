c4d6c68046fd4c4e1c4056d4516d7d19f188590c
talhaakbar22 <talhaakbar22@gmail.com>
2021-08-31 00:27:41 +0500

    pdf file show

---
+++ App.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import { StyleSheet, Dimensions, View } from 'react-native';
+
+import Pdf from 'react-native-pdf';
+
+export default class PDFExample extends React.Component {
+    render() {
+        const source = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
+        return (
+            <View style={styles.container}>
+                <Pdf
+                    source={source}
+                    onLoadComplete={(numberOfPages,filePath)=>{
+                        console.log(`number of pages: ${numberOfPages}`);
+                    }}
+                    onPageChanged={(page,numberOfPages)=>{
+                        console.log(`current page: ${page}`);
+                    }}
+                    onError={(error)=>{
+                        console.log(error);
+                    }}
+                    onPressLink={(uri)=>{
+                        console.log(`Link presse: ${uri}`)
+                    }}
+                    style={styles.pdf}/>
+            </View>
+        )
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 25,
+    },
+    pdf: {
+        flex:1,
+        width:Dimensions.get('window').width,
+        height:Dimensions.get('window').height,
+    }
+});
